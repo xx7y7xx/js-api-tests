a3735d551ed706d6d500909026068c4f467592bd
Deleted user <5204106+xx7y7xx@users.noreply.github.com>
2021-08-21 00:00:08 +0800

    Update array.js

--- test/array.js
+++ test/array.js
@@ -1,5 +1,7 @@
 var assert = require("assert");
 
+
+
 describe("Array", function() {
   describe("#forEach()", function() {
     it("js object reference", function() {
@@ -90,4 +92,15 @@ describe("Array", function() {
       });
     });
   });
+
+  describe("length", function() {
+    it("array length", function() {
+      const arr = [];
+      arr[2] = "foo";
+      arr[5] = undefined;
+      assert.equal(arr.length, 6)
+      assert.equal(arr[5], undefined)
+      assert.equal(arr[6], undefined)
+    })
+  })
 });
