425f215cd1bd23514f69d5832bf52e3c5a4db4bd
Deleted user <5204106+xx7y7xx@users.noreply.github.com>
2021-08-21 00:04:52 +0800

    Update index.js

--- test/array/index.js
+++ test/array/index.js
@@ -92,15 +92,4 @@ describe("Array", function() {
       });
     });
   });
-
-  describe("length", function() {
-    it("array length", function() {
-      const arr = [];
-      arr[2] = "foo";
-      arr[5] = undefined;
-      assert.equal(arr.length, 6)
-      assert.equal(arr[5], undefined)
-      assert.equal(arr[6], undefined)
-    })
-  })
 });
